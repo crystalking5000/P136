7c7adaeccd0e61aa6722c459a35bf78b023d1763
crystalking5000 <108053952+crystalking5000@users.noreply.github.com>
2023-04-20 12:35:35 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom()
+{
+    window.location.replace("bed.html");
+}
+
+function desk()
+{
+    window.location.replace("desk.html");
+}
+
+function Shelf()
+{
+    window.location.replace("shelf.html");
+}
+
+function side_table()
+{
+    window.location.replace("sideTable.html");
+}
